de090aae653b66ced2ab38b8d4c8d02356d8b852
Jack-777e <ttt712017@gmail.com>
2024-06-06 21:47:12 +0800

    init

--- 110360108_week14/client/ModifyStu.py
+++
@@ -1,6 +0,0 @@
-class ModifyStu:
-    @staticmethod
-    def execute(client, student_dict):
-        client.send_command('modify', student_dict)
-        raw_data = client.wait_response()
-        return raw_data["status"]
